ef2b9621fff03247b70e0c1a11d2bb15fd48601e
Deep Patel <43456929+deeprp2@users.noreply.github.com>
2021-02-28 16:09:07 -0600

    Added more filters

--- eventHandlers.js
+++ eventHandlers.js
@@ -93,6 +93,14 @@ function filter() {
                 <label for="zipcode" class="pb-1">Zipcode</label>
                 <input type="number" class="form-control" id="zipcode" placeholder="Enter Zip Code" maxlength="5" 
                     oninput="if (this.value.length > this.maxLength) this.value = this.value.slice(0, this.maxLength);">
+                    
+                <label for="community" class="pb-1 pt-3">Community</label>
+                <input type="text" class="form-control" id="community" placeholder="Fuller Park" maxlength="1000" 
+                    oninput="if (this.value.length > this.maxLength) this.value = this.value.slice(0, this.maxLength);">
+                    
+                <label for="ward" class="pb-1 pt-3">Ward</label>
+                <input type="number" class="form-control" id="ward" placeholder="10" maxlength="5" 
+                    oninput="if (this.value.length > this.maxLength) this.value = this.value.slice(0, this.maxLength);">
               </div>
                 <a class="btn btn-info" role="button" onclick="setFilteredList(document.querySelector('#filter-form'))">Submit</a>
             </form>
@@ -154,10 +162,16 @@ function setFilteredList(data) {
     let result
 
     if (data) {
-        let zipCode = data.querySelector("#zipcode").value
+        let zipCode = parseInt(data.querySelector("#zipcode").value)
+        let communityName = data.querySelector('#community').value?.toUpperCase()
+        let ward = parseInt(data.querySelector("#ward").value)
 
         result = inventory.filter(function (x) {
             if (parseInt(x.zip_code) == zipCode)
+                return x
+            else if (x.community_area_name == communityName)
+                return x
+            else if (x.ward == ward)
                 return x
         })
     } else {
@@ -242,6 +256,5 @@ function map() {
     document.getElementById('page-content').innerHTML = `
         <div id="map" style="height: 85vh; width: 85vw"></div> 
     `
-
     initMap()
 }
