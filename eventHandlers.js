c2a0e1574d48c672bc49ce980aea9f7ca6b40b92
Deep Patel <43456929+deeprp2@users.noreply.github.com>
2021-02-28 12:46:11 -0600

    added maps

--- eventHandlers.js
+++ eventHandlers.js
@@ -1,39 +1,7 @@
 let currentTab = null
 let inventory = []
+let offset = 20;
 let navAnchors = document.querySelectorAll(".navLinks")
-
-window.onload = function () {
-    if (window.localStorage.getItem('currentTab') == null) {
-        currentTab = navAnchors[0]
-        window.localStorage.setItem('currentTab', 'home')
-        inventory = getInventory()
-    } else {
-        let page = window.localStorage.getItem('currentTab')
-        currentTab = document.querySelector(`a[data-screen=${page}]`)
-        renderContent(page)
-    }
-}
-
-function getInventory() {
-    fetch('https://data.cityofchicago.org/resource/aksk-kvfp.json')
-        .then(response => response.json())
-        .then(data => {
-            return data
-        })
-}
-
-function renderContent(page) {
-    switch (page) {
-        case 'home':
-            home()
-            break;
-        case 'filter':
-            filter()
-            break;
-        case 'data':
-            data();
-    }
-}
 
 navAnchors.forEach((item) => {
     item.addEventListener('click', function (event) {
@@ -47,8 +15,49 @@ navAnchors.forEach((item) => {
     })
 })
 
+window.onload = function () {
+    getInventory()
+    if (window.localStorage.getItem('currentTab') == null) {
+        currentTab = navAnchors[0]
+        window.localStorage.setItem('currentTab', 'home')
+    } else {
+        let page = window.localStorage.getItem('currentTab')
+        currentTab = document.querySelector(`a[data-screen=${page}]`)
+        renderContent(page)
+    }
+}
+
+function getInventory() {
+    fetch('https://data.cityofchicago.org/resource/aksk-kvfp.json')
+        .then(response => response.json())
+        .then(data => {
+            inventory = data
+        })
+}
+
+function renderContent(page) {
+    switch (page) {
+        case 'home':
+            home()
+            break;
+        case 'filter':
+            filter()
+            break;
+        case 'data':
+            data();
+            break;
+        case 'map':
+            map();
+            break;
+        case 'about':
+            about();
+            break;
+    }
+}
+
 function clearPageContent() {
     document.getElementById("page-content").innerHTML = ""
+    document.getElementById('load-more')?.remove()
 }
 
 function home() {
@@ -78,18 +87,136 @@ function home() {
 function filter() {
     document.getElementById("page-content").innerHTML = `
         <div class="container-fluid" style="margin-left: auto; margin-right: auto; max-width: 50%">
-            <form class="mt-5">
+            <form class="mt-5" id="filter-form">
               <div class="form-group pb-2">
                 <label for="zipcode" class="pb-1">Zipcode</label>
                 <input type="number" class="form-control" id="zipcode" placeholder="Enter Zip Code" maxlength="5" 
                     oninput="if (this.value.length > this.maxLength) this.value = this.value.slice(0, this.maxLength);">
               </div>
-              <button type="submit" class="btn btn-primary">Submit</button>
+                <a class="btn btn-info" role="button" onclick="data(document.querySelector('#filter-form'))">Submit</a>
             </form>
         </div>
     `
 }
 
-function data() {
-    console.log("data")
+function data(data) {
+    clearPageContent()
+    let result
+
+    if (data) {
+        let zipCode = data.querySelector("#zipcode").value
+
+        result = inventory.filter(function (x) {
+            if (parseInt(x.zip_code) == zipCode)
+                return x
+        })
+    } else {
+        result = inventory
+    }
+
+    for (let i = 0; i < offset; i++) {
+        document.getElementById('page-content').innerHTML += `
+            <div class="card m-2">
+              <div class="card-body">
+               <div>
+                    ${result[i].community_area_name || "N/A"} - ${result[i]["pin"] || "N/A"}
+                </div>
+                <div>
+                    ${result[i]["address"] || "N/A"} - ${result[i]["sq_ft"] || "N/A"} sq-ft
+                </div>
+             </div> 
+            </div>
+        `
+    }
+
+    let btn = addLoadMoreButton()
+    btn.addEventListener('click', function loop () {
+        document.body.removeChild(document.getElementById('load-more'))
+        let index = offset + 1;
+
+        if (offset + 20 > inventory.length) {
+            offset = inventory.length - offset
+        } else {
+            offset += 20;
+        }
+
+        for (let i = index; i < offset; i++) {
+            document.getElementById('page-content').innerHTML += `
+            <div class="card m-2">
+              <div class="card-body">
+                 <div>
+                    ${result[i].community_area_name || "N/A"} - ${result[i]["pin"] || "N/A"}
+                </div>
+                <div>
+                    ${result[i]["address"] || "N/A"} - ${result[i]["sq_ft"] || "N/A"} sq-ft
+                </div>
+             </div> 
+            </div>
+        `
+        }
+
+        let btn = addLoadMoreButton()
+        btn.addEventListener('click', loop)
+    })
+}
+
+function addLoadMoreButton() {
+    let btn = document.createElement("btn")
+    btn.classList.add("btn")
+    btn.classList.add("btn-primary")
+    btn.innerText = 'Load More'
+    btn.id = 'load-more'
+    btn.type = "button"
+    btn.style.marginLeft = '20px'
+    btn.style.marginBottom = '20px'
+    document.body.appendChild(btn)
+    return btn
+}
+
+function about() {
+    document.getElementById('page-content').innerHTML = `
+        <h4 class="mt-4 ml-5">
+            About
+        </h4>
+        <div style="line-height: 30px">
+            A Single-Page App that allows users to search for Chicago City-Owned Land Inventory data. <br>
+            You can find more information about the data at : <a href="https://dev.socrata.com/foundry/data.cityofchicago.org/aksk-kvfp">https://dev.socrata.com/foundry/data.cityofchicago.org/aksk-kvfp </a>
+        </div>
+        <br/>
+         <h4 class="mt-4 ml-5">
+            By:
+        </h4>
+        <div style="line-height: 30px">
+           Deep Patel
+        </div>
+        <br/>
+    `
+}
+
+// Initialize and add the map
+function initMap() {
+    // The location of Uluru
+    const uluru = { lat: 41.878, lng: 87.629 };
+    // The map, centered at Uluru
+    const map = new google.maps.Map(document.getElementById("map"), {
+        zoom: 4,
+        center: uluru
+    });
+    // The marker, positioned at Uluru
+    const marker = new google.maps.Marker({
+        position: uluru,
+        map: map,
+    },
+    {
+        position: {lat: 41.887, lng: -87.713}
+    });
+}
+
+function map() {
+    clearPageContent()
+    document.getElementById('page-content').innerHTML = `
+        <div id="map" style="height: 85vh; width: 85vw"></div> 
+    `
+
+    initMap()
 }
